c6cbfb1530367fbe853627e4776212ea6327726e
AlexanderOkochutin <okochutinwork@gmail.com>
2016-10-22 22:42:59 +0300

    Union and sort

---
+++ Task06.ConsoleUI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task06.Logic;
+
+
+namespace Task06.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fst = "abcdefghijklmnopqrstuvwxyz";
+            string scnd = "abcdefghijklmnopqrstuvwxyz";
+            Console.WriteLine(ExtensionToolsForString.UnionAndSortLINQ(fst, scnd));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task06.Logic/ExtensionToolsForString.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task06.Logic
+{
+    public static class ExtensionToolsForString
+    {
+        /// <summary>
+        /// Method which make union and sort of two input strings using LINQ, elements are a-z or A-Z 
+        /// </summary>
+        /// <param name="fstString"> first input string </param>
+        /// <param name="scndString"> second input string </param>
+        /// <returns>string received from union and sort of two input strings</returns>
+        public static string UnionAndSortLINQ(string fstString, string scndString)
+        {
+            var rstString = (from s in fstString.Union(scndString) orderby s select s).ToArray();
+            return new string(rstString);
+        }
+
+     
+    }
+}
